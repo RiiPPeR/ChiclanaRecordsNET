f5eb421011af40ec9cabe7554963daa0fc7a22c8
Pablo Antonio López Butrón <panlobu@gmail.com>
2024-10-23 00:06:07 +0200

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ChiclanaRecordsNET
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
