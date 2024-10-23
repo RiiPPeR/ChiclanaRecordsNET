86fd79c367dbe86d64b2a5217284723cb8edc608
Pablo Antonio López Butrón <panlobu@gmail.com>
2024-10-23 02:11:59 +0200

    MockUp

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace ChiclanaRecordsNET
 {
@@ -19,6 +13,24 @@ namespace ChiclanaRecordsNET
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Holaa");
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
         }
     }
 }
